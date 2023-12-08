0d9712e89e95b54c7eb2f794aedb5f4d3162b693
Dragos Boboluta <dragos.boboluta@yahoo.com>
2023-12-08 11:13:56 +0200

    remove unnecessary spaces

--- BankingApp.Fundamentals.OOP/Program.cs
+++ BankingApp.Fundamentals.OOP/Program.cs
@@ -11,23 +11,25 @@ using (var scope = container.BeginLifetimeScope())
     var reporter = scope.Resolve<IReporter>();
     var creditService = scope.Resolve<ICreditService>();
     
-    User user = new User("Dragos");
+    User user1 = new User("Dragos");
     User user2 = new User("Daniel");
 
     CreditAccount creditAccount = new CreditAccount(2000,CreditCategory.PersonalLoan);
     CreditAccount creditAccount2 = new CreditAccount(5000, CreditCategory.HomeLoan);
 
-    creditService.AssignCredit(user, creditAccount);
-    creditService.AssignCredit(user, creditAccount2);
-
+    creditService.AssignCredit(user1, creditAccount);
+    creditService.AssignCredit(user1, creditAccount2);
 
     CreditAccount creditAccount3 = new CreditAccount(7000, CreditCategory.PersonalLoan);
     CreditAccount creditAccount4 = new CreditAccount(1000, CreditCategory.HomeLoan);
+    CreditAccount creditAccount5 = new CreditAccount(500, CreditCategory.AutoLoan);
+    CreditAccount creditAccount6 = new CreditAccount(6000, CreditCategory.BusinessLoan);
 
     creditService.AssignCredit(user2, creditAccount3);
     creditService.AssignCredit(user2, creditAccount4);
-    
+    creditService.AssignCredit(user2, creditAccount5);
+    creditService.AssignCredit(user2, creditAccount6);
 
-    reporter.DisplayCreditInformation(user);
+    reporter.DisplayCreditInformation(user1);
     reporter.DisplayCreditInformation(user2);
 }
