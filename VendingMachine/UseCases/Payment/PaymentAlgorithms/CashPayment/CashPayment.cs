5cf32a0dfda88adafef1993fb3bf125e20e8f044
CHATGPT\drago <dragos.boboluta@yahoo.com>
2024-01-13 21:53:09 -1200

    implement cash payment run metrhod

--- VendingMachine/UseCases/Payment/PaymentAlgorithms/CashPayment/CashPayment.cs
+++ VendingMachine/UseCases/Payment/PaymentAlgorithms/CashPayment/CashPayment.cs
@@ -1,4 +1,7 @@
 ﻿
+using Nagarro.VendingMachine.PresentationLayer;
+using System;
+
 namespace Nagarro.VendingMachine.UseCases.Payment.PaymentAlgorithms.CashPayment
 {
     public class CashPayment : IPaymenthAlgorithm
@@ -11,7 +14,36 @@ namespace Nagarro.VendingMachine.UseCases.Payment.PaymentAlgorithms.CashPayment
 
         public void Run(float price)
         {
+            var terminal = new CashPaymentTerminal();
 
+            float? balance = 0;
+            float? value ;
+            while (price > 0)
+            {
+                try
+                {
+                    value = terminal.AskForMoney();
+                    balance += value;
+
+                   
+                        price -= value.Value;
+                        if (price > 0)
+                        {
+                            Console.WriteLine($"Remainding price : {price} Ron");
+                        }
+                }
+                catch (Exception ex)
+                {
+                    terminal.GiveBackChange(balance.Value);
+                    throw;
+                }
+            }
+            if (price < 0)
+            {
+                float change = -price;
+                float truncatedchange = (float)(Math.Floor(change * 1000) / 1000);
+                terminal.GiveBackChange(truncatedchange);
+            }
         }
     }
 }
