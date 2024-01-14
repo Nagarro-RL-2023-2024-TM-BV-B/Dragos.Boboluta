72f92f0b8ec1542ba51c2d17edd3fc171037a3e1
CHATGPT\drago <dragos.boboluta@yahoo.com>
2024-01-13 21:57:25 -1200

    check if inserted money is a monetary value

--- VendingMachine/UseCases/Payment/PaymentAlgorithms/CashPayment/CashPayment.cs
+++ VendingMachine/UseCases/Payment/PaymentAlgorithms/CashPayment/CashPayment.cs
@@ -17,7 +17,7 @@ namespace Nagarro.VendingMachine.UseCases.Payment.PaymentAlgorithms.CashPayment
             var terminal = new CashPaymentTerminal();
 
             float? balance = 0;
-            float? value ;
+            float? value;
             while (price > 0)
             {
                 try
@@ -25,12 +25,20 @@ namespace Nagarro.VendingMachine.UseCases.Payment.PaymentAlgorithms.CashPayment
                     value = terminal.AskForMoney();
                     balance += value;
 
-                   
+                    if (IsMonetaryValue(value.Value * 100))
+                    {
                         price -= value.Value;
                         if (price > 0)
                         {
                             Console.WriteLine($"Remainding price : {price} Ron");
                         }
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Enter a valid monetary value ");
+                        Console.WriteLine();
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -44,6 +52,25 @@ namespace Nagarro.VendingMachine.UseCases.Payment.PaymentAlgorithms.CashPayment
                 float truncatedchange = (float)(Math.Floor(change * 1000) / 1000);
                 terminal.GiveBackChange(truncatedchange);
             }
+        }
+        private static bool IsMonetaryValue(float price)
+        {
+            bool isValid = Enum.IsDefined(typeof(MonetaryValue), (int)price);
+            return isValid ? true : false;
+        }
+        private enum MonetaryValue
+        {
+            UnBan = 1,
+            CinciBani = 5,
+            ZeceBani = 10,
+            CincizeciBani = 50,
+            UnLeu = 100,
+            CinciLei = 500,
+            ZeceLei = 1000,
+            CincizecDeiLei = 5000,
+            OSutadeLei = 10000,
+            DouaSuteDeLei = 20000,
+            CinciSuteDeLei = 50000
         }
     }
 }
