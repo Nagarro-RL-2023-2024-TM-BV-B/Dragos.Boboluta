dc0b26a4e50a61c9645f4cac7545213daaaef46f
CHATGPT\drago <dragos.boboluta@yahoo.com>
2024-01-13 21:54:59 -1200

    check if a card number is valid using luhn algorithm

--- VendingMachine/UseCases/Payment/PaymentAlgorithms/CardPayment/CardPayment.cs
+++ VendingMachine/UseCases/Payment/PaymentAlgorithms/CardPayment/CardPayment.cs
@@ -17,14 +17,46 @@ namespace Nagarro.VendingMachine.UseCases.Payment.PaymentAlgorithms.CardPayment
         {
             var terminal = new CardPaymentTerminal();
             string response = terminal.AskForCardNumber();
-            bool isValid = true ;
+            bool isValid = IsValidLuhn(response) ;
             if (!isValid)
             {
                 throw new Exception("Bad card number");
                 throw new CancelException();
             }
         }
-      
+        private static bool IsValidLuhn(string cardNumber)
+        {
+            char[] charArray = cardNumber.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            string reversedCardNumber = new string(charArray);
+
+            int sum = 0;
+            bool doubleDigit = false;
+
+            foreach (char digit in reversedCardNumber)
+            {
+                if (char.IsDigit(digit))
+                {
+                    int digitValue = digit - '0';
+
+                    if (doubleDigit)
+                    {
+                        digitValue *= 2;
+
+                        if (digitValue > 9)
+                        {
+                            digitValue -= 9;
+                        }
+                    }
+                    sum += digitValue;
+                    doubleDigit = !doubleDigit;
+                }
+            }
+            return sum % 10 == 0;
+        }
+
     }
 
 }
