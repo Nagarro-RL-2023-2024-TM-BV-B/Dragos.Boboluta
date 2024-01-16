ddf8b310148b32df9ef07ff0106f75a381ecc18a
CHATGPT\drago <dragos.boboluta@yahoo.com>
2024-01-15 20:43:07 -1200

    implement cash payment run method and cash give back change

--- VendingMachine/PresentationLayer/CashPaymentTerminal.cs
+++ VendingMachine/PresentationLayer/CashPaymentTerminal.cs
@@ -1,10 +1,16 @@
-﻿namespace Nagarro.VendingMachine.PresentationLayer
+﻿using System;
+
+namespace Nagarro.VendingMachine.PresentationLayer
 {
     internal class CashPaymentTerminal : DisplayBase
     {
         public float? AskForMoney()
         {
             return 200;
+        }
+        public void GiveBackChange(float balance)
+        {
+           
         }
     }
 }
