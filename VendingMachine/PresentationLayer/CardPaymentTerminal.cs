4df1356cd235226ad3e708f214e3314792fbf49c
CHATGPT\drago <dragos.boboluta@yahoo.com>
2024-01-15 20:54:23 -1200

    working card payment terminal

--- VendingMachine/PresentationLayer/CardPaymentTerminal.cs
+++ VendingMachine/PresentationLayer/CardPaymentTerminal.cs
@@ -1,10 +1,20 @@
-﻿namespace Nagarro.VendingMachine.PresentationLayer
+﻿using System;
+
+namespace Nagarro.VendingMachine.PresentationLayer
 {
     internal class CardPaymentTerminal : DisplayBase
     {
         public string AskForCardNumber()
         {
-            return "12341511";
+            Console.WriteLine();
+            Display("Enter a credit card number (Enter to cancel): ", ConsoleColor.Cyan);
+
+            string creditCardNumber = Console.ReadLine();
+            Console.WriteLine();
+            if (string.IsNullOrEmpty(creditCardNumber))
+                throw new CancelException();
+
+            return creditCardNumber;
         }
     }
 }
