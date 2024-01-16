0b83424e4aae269a7ba0b17eb0d577ae2a55b6c0
CHATGPT\drago <dragos.boboluta@yahoo.com>
2024-01-16 07:36:53 -1200

    initial files

--- BankingApp.Fundamentals.OOP/Exceptions/AccountNumberException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BankingApp.Fundamentals.OOP.Exceptions
-{
-    public class AccountNumberException : Exception
-    {
-        public AccountNumberException() : base($"Account Number must contain 12 characters") { }
-    }
-}
